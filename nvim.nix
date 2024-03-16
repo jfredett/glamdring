5e1d30137bbe166101741ba9aa7898f3d46c516e
Joe Fredette <jfredett@gmail.com>
2024-03-15 23:08:02 -0400

    remove rnix (deprecated)

--- nvim.nix
+++ nvim.nix
@@ -157,7 +157,6 @@
 
             lspconfig.rust_analyzer.setup{}
             lspconfig.lua_ls.setup{}
-            lspconfig.rnix.setup{}
 
             if not configs.ruby_lsp then
               configs.ruby_lsp = {
