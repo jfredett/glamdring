9d048c9c0d3725e4f1c02814cb94d847ecb6d522
Joe Fredette <jfredett@gmail.com>
2024-07-26 14:48:59 -0400

    refactor ssh-config

--- modules/ssh/default.nix
+++ modules/ssh/default.nix
@@ -7,22 +7,16 @@
     # TODO: Turnkey service should pull these keys to /run/keys, owned by this user
     programs.ssh = let
       ssh_dir = "~/.ssh";
+      mkLocalDomain = {
+        checkHostIP = false;
+        identityFile = "${ssh_dir}/archimedes";
+      };
     in {
       enable = true;
       matchBlocks = {
-        "pinky" = {
-          identityFile = "${ssh_dir}/archimedes";
-        };
-
-        "*.pandemon.ium" = {
-          checkHostIP = false;
-          identityFile = "${ssh_dir}/archimedes";
-        };
-
-        "*.emerald.city" = {
-          checkHostIP = false;
-          identityFile = "${ssh_dir}/archimedes";
-        };
+        "*.pandemon.ium" = mkLocalDomain;
+        "*.emerald.city" = mkLocalDomain;
+        "*.canon" = mkLocalDomain;
 
         "github.work" = {
           hostname = "github.com";
