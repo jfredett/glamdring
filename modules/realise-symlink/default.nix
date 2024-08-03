28139c109c0e2b9219664dad1e77d50f2608ade8
Joe Fredette <jfredett@gmail.com>
2024-08-03 00:37:41 -0400

    stole a good idea

--- modules/realise-symlink/default.nix
+++ modules/realise-symlink/default.nix
@@ -30,6 +30,6 @@ in {
   };
 
   config = mkIf config.glamdring.realise-symlink.enable {
-    home.packages = [ realise-symlink ];
+    home.packages = lib.mkIf glamdring.realise-symlink.enable [ realise-symlink ];
   };
 }
